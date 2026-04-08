b0e5da7c07887ff24904cb14478ed425e4888322
Atanu Guchhait <atanuguchhait721437@gmail.com>
2026-04-08 14:40:59 +0530

    use chroma metadata filter

--- src/retrievers/metadata_filter_retriever.py
+++ src/retrievers/metadata_filter_retriever.py
@@ -3,9 +3,6 @@ from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from src.retrievers.chroma_metadata_filter import build_chroma_filter
 
-
-
-
 # *************************** PRE-FILTER ***************************
 def retrieve_with_filter(
     vectordb: Chroma,
@@ -16,9 +13,13 @@ def retrieve_with_filter(
     """
     Retrieve documents using metadata filtering (pre-filtering)
     """
+    
+    #  Use the Chroma filter right here to translate the dictionary!
+    chroma_formatted_filter = build_chroma_filter(filters) if filters else None
 
+    #  Pass the translated filter to the database
     return vectordb.similarity_search(
         query=query,
         k=k,
-        filter=filters if filters else None
+        filter=chroma_formatted_filter 
     )
